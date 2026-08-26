17a8f3dfdf5fd04c4a032c441773b81f84a2fd8f
Arul Deshwal <144941453+Aruldeshwal@users.noreply.github.com>
2026-08-26 13:56:56 +0530

    Time: 0 ms (100%), Space: 11.3 MB (94.28%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -2,24 +2,26 @@ class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int n = obstacleGrid.size(), m = obstacleGrid[0].size();
-        vector<vector<long long>> dp(n + 1, vector<long long> (m + 1, 0));
+        vector<long long> currRow(m + 1, 0);
+        vector<long long> nextRow(m + 1, 0);
         if(obstacleGrid[n - 1][m - 1] != 1) {
-            dp[n - 1][m - 1] = 1;
+            currRow[m - 1] = 1;
         }
         for(int i = n - 1; i >= 0; i--) {
             for(int j = m - 1; j >= 0; j--) {
                 if(i == n - 1 && j == m - 1) continue;
                 
                 if(obstacleGrid[i][j] == 1) {
-                    dp[i][j] = 0;
+                    currRow[j] = 0;
                     continue;
                 }
-                long long rightWay = dp[i][j + 1];
-                long long downWay = dp[i + 1][j];
-                dp[i][j] = rightWay + downWay;
+                long long rightWay = currRow[j + 1];
+                long long downWay = nextRow[j];
+                currRow[j] = rightWay + downWay;
             }
+            nextRow = currRow;
         }
 
-        return dp[0][0];
+        return nextRow[0];
     }
 };
