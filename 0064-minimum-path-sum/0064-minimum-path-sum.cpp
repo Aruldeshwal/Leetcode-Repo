99508e753cb9c3e754a4430a8c5758780038e249
Arul Deshwal <144941453+Aruldeshwal@users.noreply.github.com>
2026-08-26 14:58:46 +0530

    Time: 4 ms (15.32%), Space: 16.2 MB (72.5%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
-    int solve(vector<vector<int>> &grid, int i, int j, vector<vector<int>> &dp) {
-        int n = grid.size(), m = grid[0].size();
-        if(i == n || j == m) return dp[i][j] = INT_MAX;
-        if(i == n - 1 && j == m - 1) return dp[i][j] = grid[i][j];
-        if(dp[i][j] != -1) return dp[i][j];
-        return dp[i][j] = grid[i][j] + min(solve(grid, i + 1, j, dp), solve(grid, i, j + 1, dp));
-    }
     int minPathSum(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
         vector<vector<int>> dp(n + 1, vector<int>(m + 1, -1));
-        return solve(grid, 0, 0, dp);
+        
+        for(int i = n - 1; i >= 0; i--) {
+            for(int j = m - 1; j >= 0; j--) {
+                if(dp[i + 1][j] != -1 && dp[i][j + 1] != -1) {
+                    dp[i][j] = grid[i][j] + min(dp[i + 1][j], dp[i][j + 1]);
+                } 
+                else if(dp[i + 1][j] == -1 && dp[i][j + 1] != -1) {
+                    dp[i][j] = grid[i][j] + dp[i][j + 1];
+                }
+                else if(dp[i + 1][j] != -1 && dp[i][j + 1] == -1) {
+                    dp[i][j] = grid[i][j] + dp[i + 1][j];
+                }
+                else dp[i][j] = grid[i][j];
+                
+            }
+        }
+        return dp[0][0];
     }
 };
