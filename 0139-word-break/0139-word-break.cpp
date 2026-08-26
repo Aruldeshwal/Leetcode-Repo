4c031acbe5285be00f0cf6ecefd2139e133e8c0a
Arul Deshwal <144941453+Aruldeshwal@users.noreply.github.com>
2026-08-26 15:56:02 +0530

    Time: 0 ms (100%), Space: 10.7 MB (83.22%) - LeetHub

--- 0139-word-break/0139-word-break.cpp
+++ 0139-word-break/0139-word-break.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
-    bool solve(string s, vector<string> &wordDict, int index, vector<int> &dp) {
-        if(index == s.size()) return dp[index] = false;
-        if(dp[index] != -1) return dp[index];
-        bool ans = false;
-        for(string word : wordDict) {
-            int i = 0;
-            while(i + index < s.size() && i < word.size() && s[i + index] == word[i]) {
-                i++;
-            }
-            if(i != word.size()) continue;
-            if(i + index == s.size()) ans = true;
-            else ans = ans || solve(s, wordDict, index + i, dp);
-        }
-        return dp[index] = ans;
-    }
-    bool wordBreak(string s, vector<string>& wordDict) {
-        int n = s.size();
-        vector<int> dp(n + 1, -1);
-        return solve(s, wordDict, 0, dp);
-    }
+
+    bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size();
+        vector<int> dp(n + 1, 0);
+        dp[n] = 1;
+        for(int index = n - 1; index >= 0; index--) {
+            for(string word : wordDict) {
+                int i = 0;
+                int m = word.size();
+                while(i + index < n && i < m && s[i + index] == word[i]) {
+                    i++;
+                }
+                if(i != m) continue;
+                if(dp[index + i]) {
+                    dp[index] = 1;
+                    break;
+                }
+            }
+        }
+        return dp[0];
+    }
 };
