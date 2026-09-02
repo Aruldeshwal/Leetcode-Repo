ed772c8a9bec3bf1572ab643469af42cdd6e5512
Arul Deshwal <144941453+Aruldeshwal@users.noreply.github.com>
2026-09-02 17:45:28 +0530

    Time: 259 ms (56.08%), Space: 343 MB (5.01%) - LeetHub

--- 4040-minimum-operations-to-form-subset-sum-i/4040-minimum-operations-to-form-subset-sum-i.cpp
+++ 4040-minimum-operations-to-form-subset-sum-i/4040-minimum-operations-to-form-subset-sum-i.cpp
@@ -1,36 +1,48 @@
 class Solution {
 public:
-    int solve(vector<int> &nums, int sum, int i, vector<vector<int>> &dp) {
-        if(sum == 0) return 0;
-        if(i >= nums.size()) return INT_MAX;
-        if(sum < 0) return INT_MAX;     
-        if(dp[i][sum] != -1) return dp[i][sum];
-        int ans = INT_MAX;
-        ans = min(ans, solve(nums, sum, i + 1, dp));
-        int val = nums[i];
-        int opp = 0; 
-        while(val){    
-            val /= 2; 
-            opp++;
-            int rr = solve(nums, sum - val, i + 1, dp); 
-            if(rr != INT_MAX) rr = rr + opp; 
-            ans = min(ans, rr);
-        }
-        val = nums[i];
-        opp = 0; 
-        while(val <= 5000){
-            int rr = solve(nums, sum - val, i + 1, dp);
-            if(rr != INT_MAX) rr = rr + opp;  
-            ans = min(ans, rr);
-            opp++; 
-            val = val * 2; 
-        }
-        return dp[i][sum] = ans;
-    }
     int minOperations(vector<int>& nums, int sum) {
-        vector<vector<int>> dp(nums.size() + 1, vector<int> (5002, -1));
-        int ans = solve(nums, sum, 0, dp);
-        if(ans == INT_MAX) return -1;
-        return ans;
+        int n = nums.size();
+        vector<vector<int>> dp(n + 1, vector<int> (5002, INT_MAX));
+        for(int i = 0; i <= n; i++) {
+            dp[i][0] = 0;
+        }
+        for(int i = n - 1; i >= 0; i--) {
+            for(int j = 0; j <= sum; j++) {
+                int ans = INT_MAX;
+                ans = min(ans, dp[i + 1][j]);
+                int val = nums[i];
+                int opp = 0; 
+                while(val > 0) {    
+                    val /= 2; 
+                    opp++;
+                    if (j >= val) { 
+                        int rr = dp[i + 1][j - val]; 
+                        if(rr != INT_MAX) rr = rr + opp; 
+                        ans = min(ans, rr);
+                    }
+                }
+                val = nums[i];
+                opp = 0; 
+                while(val <= 5000){
+                    if (j >= val) {
+                    int rr = dp[i + 1][j - val];
+                    if(rr != INT_MAX) rr = rr + opp;  
+                    ans = min(ans, rr);
+                    } 
+                    else {
+                        break; 
+                    }
+                    
+                    if (val == 0) break; 
+                    
+                    opp++; 
+                    val = val * 2;
+                    
+                }
+                dp[i][j] = ans;
+            }
+        }
+        if(dp[0][sum] == INT_MAX) return -1;
+        return dp[0][sum];
     }
 };
